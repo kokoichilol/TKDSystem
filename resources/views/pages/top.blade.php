7888c408a099fd73b973200277c6cbdbab2ed865
KAZUKI <KAZUKI@DESKTOP-130B55I>
2020-08-03 23:02:19 +0900

    メインテンプレートの作成

---
+++ resources/views/pages/top.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.mainLayout')
+
+@section('title')
+メインメニュー
+@endsection
+
+@section('content')
+コンテンツ表示テスト
+@endsection
+
+@section('pageScript')
+alert('test');
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages/top');
+});
+
+Route::get('/main', function () {
+    return view('mainLayout');
 });
