f8b3154681acf2ad6ef630ade2de54f61e476e6c
KAZUKI <KAZUKI@DESKTOP-130B55I>
2020-08-05 18:24:49 +0900

    メニューバーの部品化

--- resources/views/layouts/mainLayout.blade.php
+++ resources/views/layouts/mainLayout.blade.php
@@ -1,21 +1,13 @@
 <html>
 <head>
-	<title>TKDSystem - @yield('title')</title>
-	<script src="{{asset('/js/jquery-3.5.1.min.js')}}"></script>
-	<link rel="stylesheet" href="{{asset('/css/main.css')}}">
-	@yield('pageCss')
+<title>TKDSystem - @yield('title')</title>
+<script src="{{asset('/js/jquery-3.5.1.min.js')}}"></script>
+<link rel="stylesheet" href="{{asset('/css/main.css')}}">
+@yield('pageCss')
 </head>
 <body>
-	<nav>
-		<ul>
-			<li class="current"><a href="#">模試</a></li>
-			<li><a href="#">履歴</a></li>
-			<li><a href="#">マスタ登録</a></li>
-		</ul>
-	</nav>
-	<div class="main">
-		@yield('content')
-	</div>
+	@include('layouts.menuBar')
+	<div class="main">@yield('content')</div>
 	<script>
 		@yield('pageScript')
  	</script>
---
+++ resources/views/layouts/menuBar.blade.php
@@ -0,0 +1,7 @@
+<nav>
+	<ul>
+		<li class="current"><a href="#">模試</a></li>
+		<li><a href="#">履歴</a></li>
+		<li><a href="#">マスタ登録</a></li>
+	</ul>
+</nav>
